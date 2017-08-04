d04223fdd04da8cdd8b92d2689ff8231176b1120
Ajit Shinde <ajitdev25@gmail.com>
2017-08-04 18:03:22 +0530

    Moving the pre-render logic of removing extra form elements to alter function.

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -37,8 +37,6 @@ class SearchForm extends FormBase {
     $form['#action'] = 'search';
     $form['#method'] = 'get';
 
-    $form['#pre_render'][] = 'simple_gse_search_form_pre_render';
-
     return $form;
   }
 
@@ -47,19 +45,6 @@ class SearchForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Intentionally leave this empty.
-  }
-
-  /**
-   * Pre-render function for the search form.
-   *
-   * Since the form GETs rather than POSTs, this will remove a bunch of Drupal-
-   * specific parameters.
-   */
-  function simple_gse_search_form_pre_render($form) {
-    unset($form['form_token']);
-    unset($form['form_build_id']);
-    unset($form['form_id']);
-    return $form;
   }
 
 }
